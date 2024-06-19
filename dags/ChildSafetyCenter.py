23bf57ca3d65ba85a1d08509e06688b72bb4ba6f
Yonggu Choi <koir1100@gmail.com>
2024-06-19 23:26:13 +0900

    fix. BaseSettingFiles Dag merge into ChildSafetyCenter Dag
    - Dag 통합 및 TaskGroup으로 분리
    - 전역변수 관련 통일 및 마이너 소스 정리

--- dags/ChildSafetyCenter.py
+++ dags/ChildSafetyCenter.py
@@ -1,10 +1,12 @@
 from airflow import DAG
-from airflow.models import Variable
 from airflow.decorators import task
-from airflow.providers.postgres.hooks.postgres import PostgresHook
 
+from airflow.operators.python import PythonOperator
 from airflow.providers.common.sql.operators.sql import SQLExecuteQueryOperator
 from airflow.providers.amazon.aws.transfers.s3_to_redshift import S3ToRedshiftOperator
+
+from airflow.utils.task_group import TaskGroup
+from airflow.models import Variable
 
 from datetime import datetime
 from datetime import timedelta
@@ -22,6 +24,7 @@ import pandas as pd
 import time
 from collections import defaultdict
 
+from plugins import gsheet
 from plugins import s3
 from plugins import update_sido
 
@@ -29,6 +32,73 @@ from plugins import update_sido
 s3_conn_id = "aws_default"
 s3_bucket = "litchiimg"
 data_dir = Variable.get("DATA_DIR")
+
+sheets = [
+    {
+        "url": f"{Variable.get('gsheet_url_zip')}",
+        "tab": "zipcode_new",
+        "schema": "hhee2864",
+        "table": "zipcode_new",
+        "table_schema": "sido varchar(6), zip_code varchar(50)"
+    },
+    {
+        "url": f"{Variable.get('gsheet_url_zip')}",
+        "tab": "zipcode_old",
+        "schema": "hhee2864",
+        "table": "zipcode_old",
+        "table_schema": "sido varchar(6), zip_code varchar(50)"
+    },
+    {
+        "url": f"{Variable.get('gsheet_url_name_list')}",
+        "tab": "localname_list",
+        "schema": "hhee2864",
+        "table": "localname_list",
+        "table_schema": "local_name varchar(24), sido varchar(6)"
+    },
+    {
+        "url": f"{Variable.get('gsheet_url_name_list')}",
+        "tab": "localname_iso_list",
+        "schema": "hhee2864",
+        "table": "localname_iso_list",
+        "table_schema": "sido varchar(6), iso_code varchar(10)"
+    },
+]
+
+url = "https://www.safe182.go.kr/api/lcm/safeMap.do"
+schema = 'hhee2864'
+table_info = {
+    "table_name": "child_safety_center",
+    "table_schema": [
+        "serial_no int primary key",
+        "place_name varchar(150) not null",
+        "sido varchar(30) not null",
+        "sigungu varchar(30)",
+        "address varchar(1000)",
+        "zip_code varchar(10)",
+        "phone_number varchar(20)"
+    ]
+}
+
+def download_tab_in_gsheet(**context):
+    url = context["params"]["url"]
+    tab = context["params"]["tab"]
+    table = context["params"]["table"]
+
+    gsheet.get_google_sheet_to_csv(
+        url,
+        tab,
+        data_dir + '/' + '{}.csv'.format(table)
+    )
+
+
+def copy_to_s3(**context):
+    table = context["params"]["table"]
+    s3_key = context["params"]["s3_key"]
+
+    local_files_to_upload = [data_dir + '/' + '{}.csv'.format(table)]
+    replace = True
+
+    s3.upload_to_s3(s3_conn_id, s3_bucket, s3_key, local_files_to_upload, replace)
 
 
 def get_raw_data_count(url, params, is_local):
@@ -82,7 +152,7 @@ def update_sido_data(path):
     merged_df = update_sido.fill_missing_sido(merged_df)
     logging.info(f"missing counts: {merged_df[merged_df['sido'].isna()].count()}")
 
-    path = update_sido.save_to_csv(merged_df, Variable.get("DATA_DIR") + '/' + 'updated_safety_center_list.csv')
+    path = update_sido.save_to_csv(merged_df, data_dir + '/' + 'updated_safety_center_list.csv')
     return (path, merged_df["serial_no"].count())
 
 @task
@@ -161,56 +231,99 @@ with DAG(
     template_searchpath=[f"{Variable.get('INCLUDE_DIR')}"],
 ) as dag:
 
-    url = "https://www.safe182.go.kr/api/lcm/safeMap.do"
-    schema = 'hhee2864'
-    table_info = {
-        "table_name": "child_safety_center",
-        "table_schema": [
-            "serial_no int primary key",
-            "place_name varchar(150) not null",
-            "sido varchar(30) not null",
-            "sigungu varchar(30)",
-            "address varchar(1000)",
-            "zip_code varchar(10)",
-            "phone_number varchar(20)"
-        ]
-    }
-
-    table_setting_task = SQLExecuteQueryOperator(
-        task_id = 'table_setting_{}_in_redshfit'.format(table_info["table_name"]),
-        conn_id = "redshift_dev_db",
-        sql = f"""
-        DROP TABLE IF EXISTS {table_info["table_name"]};
-        CREATE TABLE {table_info["table_name"]} ({",".join(table_info["table_schema"])});""",
-        autocommit = True,
-        split_statements = True,
-        return_last = False,
+    with TaskGroup(
+        group_id = "BaseSettingTaskGroup",
+        tooltip = "Execute before Main Tasks due to base setting",
         dag = dag
-    )
+    ) as base_setting_taskgroup:
+        for sheet in sheets:
+            table_setting_task = SQLExecuteQueryOperator(
+                task_id = 'table_setting_{}_in_redshfit'.format(sheet["table"]),
+                conn_id = "redshift_dev_db",
+                sql = f"""
+                DROP TABLE IF EXISTS {sheet["table"]};
+                CREATE TABLE {sheet["table"]} ({sheet["table_schema"]});
+                """,
+                autocommit = True,
+                split_statements = True,
+                return_last = False,
+                dag = dag
+            )
 
-    load_task = S3ToRedshiftOperator(
-        task_id = 'run_copy_sql_{}'.format(table_info["table_name"]),
-        s3_bucket = s3_bucket,
-        s3_key = "upload" + "/" + "updated_safety_center_list.csv",
-        schema = schema,
-        table = table_info["table_name"],
-        column_list = ["serial_no", "place_name", "phone_number", "address", "zip_code", "sigungu", "sido"],
-        copy_options = ["csv", "IGNOREHEADER AS 1", "QUOTE AS '\"'", "DELIMITER ','"],
-        method = 'REPLACE',
-        redshift_conn_id = "redshift_dev_db",
-        aws_conn_id = 'aws_default',
+            download_task = PythonOperator(
+                task_id = 'download_{}_in_gsheet'.format(sheet["table"]),
+                python_callable = download_tab_in_gsheet,
+                params = sheet,
+                dag = dag)
+
+            s3_key = "upload" + "/" + sheet["schema"] + "_" + sheet["table"] + ".csv"
+
+            copy_task = PythonOperator(
+                task_id = 'copy_{}_to_s3'.format(sheet["table"]),
+                python_callable = copy_to_s3,
+                params = {
+                    "table": sheet["table"],
+                    "s3_key": s3_key
+                },
+                dag = dag)
+
+            run_copy_sql = S3ToRedshiftOperator(
+                task_id = 'run_copy_sql_{}'.format(sheet["table"]),
+                s3_bucket = s3_bucket,
+                s3_key = s3_key,
+                schema = sheet["schema"],
+                table = sheet["table"],
+                copy_options = ["csv", "IGNOREHEADER AS 1", "QUOTE AS '\"'", "DELIMITER ','"],
+                method = 'REPLACE',
+                redshift_conn_id = "redshift_dev_db",
+                aws_conn_id = 'aws_default',
+                dag = dag
+            )
+
+            table_setting_task >> download_task >> copy_task >> run_copy_sql
+
+    with TaskGroup(
+        group_id = "MainTaskGroup",
+        tooltip = "Police 182 API data ETL and ELT",
         dag = dag
-    )
+    ) as main_taskgroup:
+        table_setting_task = SQLExecuteQueryOperator(
+            task_id = 'table_setting_{}_in_redshfit'.format(table_info["table_name"]),
+            conn_id = "redshift_dev_db",
+            sql = f"""
+            DROP TABLE IF EXISTS {table_info["table_name"]};
+            CREATE TABLE {table_info["table_name"]} ({",".join(table_info["table_schema"])});""",
+            autocommit = True,
+            split_statements = True,
+            return_last = False,
+            dag = dag
+        )
 
-    elt_task = SQLExecuteQueryOperator(
-        task_id = 'elt_task',
-        conn_id = "redshift_dev_db",
-        sql = "child_safety_center_summary.sql",
-        autocommit = True,
-        split_statements = True,
-        return_last = False,
-        dag = dag
-    )
+        load_task = S3ToRedshiftOperator(
+            task_id = 'run_copy_sql_{}'.format(table_info["table_name"]),
+            s3_bucket = s3_bucket,
+            s3_key = "upload" + "/" + "updated_safety_center_list.csv",
+            schema = schema,
+            table = table_info["table_name"],
+            column_list = ["serial_no", "place_name", "phone_number", "address", "zip_code", "sigungu", "sido"],
+            copy_options = ["csv", "IGNOREHEADER AS 1", "QUOTE AS '\"'", "DELIMITER ','"],
+            method = 'REPLACE',
+            redshift_conn_id = "redshift_dev_db",
+            aws_conn_id = 'aws_default',
+            dag = dag
+        )
 
-    extract_transform_task = transform(extract(url))
-    extract_transform_task >> table_setting_task >> load_task >> elt_task
+        elt_task = SQLExecuteQueryOperator(
+            task_id = 'elt_task',
+            conn_id = "redshift_dev_db",
+            sql = "child_safety_center_summary.sql",
+            autocommit = True,
+            split_statements = True,
+            return_last = False,
+            dag = dag
+        )
+
+        extract_transform_task = transform(extract(url))
+        extract_transform_task >> table_setting_task >> load_task >> elt_task
+
+    base_setting_taskgroup >> main_taskgroup
